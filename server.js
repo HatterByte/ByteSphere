29f32be1c0f328b982b61c31c1a1979af2c41688
Anubhav Chandak <anubhavchandak@gmail.com>
2025-05-23 11:58:21 +0530

    Deployement fixes

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 //Connecting database
 connectDB();
 
 //Init Middleware
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 
-app.use(cors({
-  origin: 'https://bytesphere-d4zj.onrender.com',
-  credentials: true
-}));
-app.get('/',(req,res)=>res.send('API Running'));
+app.use(
+  cors({
+    origin: "https://bytesphere-3n3r.onrender.com",
+    credentials: true,
+  })
+);
+app.get("/", (req, res) => res.send("API Running"));
 
 //Define routes
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/profile',require('./routes/api/profile'));
-app.use('/api/posts',require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
